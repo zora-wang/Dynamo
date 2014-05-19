7759955ae3e697e688bf8e60ce6dbf3511e93339
Stephen Elliott <steell1890@gmail.com>
2014-05-19 15:42:54 -0400

    ManipulatorDaemon

---
+++ src/DynamoCore/Manipulation/ManipulatorDaemon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dynamo.Models;
+
+namespace Dynamo.Manipulation
+{
+    public class ManipulatorDaemon
+    {
+        private readonly Dictionary<Type, INodeManipulatorCreator> registeredManipulators;
+
+        private readonly Dictionary<Guid, IDisposable> activeManipulators =
+            new Dictionary<Guid, IDisposable>();
+
+        private ManipulatorDaemon(Dictionary<Type, INodeManipulatorCreator> manips)
+        {
+            registeredManipulators = manips;
+        }
+
+        public static ManipulatorDaemon Create(IManipulatorDaemonInitializer initializer)
+        {
+            return new ManipulatorDaemon(initializer.GetManipulators());
+        }
+
+        public void CreateManipulator(NodeModel model)
+        {
+            INodeManipulatorCreator creator;
+            if (registeredManipulators.TryGetValue(model.GetType(), out creator))
+                activeManipulators[model.GUID] = creator.Create(model, new DynamoContext());
+        }
+
+        public void KillManipulator(NodeModel model)
+        {
+            IDisposable disposable;
+            if (activeManipulators.TryGetValue(model.GUID, out disposable))
+                disposable.Dispose();
+        }
+    }
+}
