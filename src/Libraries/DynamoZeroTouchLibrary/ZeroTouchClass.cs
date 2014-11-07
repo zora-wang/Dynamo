7caee3ccb4f11c82a0a0c2c4a9162fff4cea21e5
Ian Keough <ian.keough@autodesk.com>
2014-11-07 15:59:29 -0800

    Add DynamoZeroTouchLibrary template project.

---
+++ src/Libraries/DynamoZeroTouchLibrary/ZeroTouchClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.DesignScript.Geometry;
+
+namespace DynamoZeroTouchLibrary
+{
+    public class ZeroTouchClass : IDisposable
+    {
+        /// <summary>
+        /// An example private constructor.
+        /// </summary>
+        private ZeroTouchClass()
+        {
+            // Default private constructor.    
+        }
+
+        /// <summary>
+        /// An example public static constructor.
+        /// </summary>
+        /// <param name="name">An example parameter with a default value.</param>
+        /// <returns>A ZeroTouchClass object.</returns>
+        public static ZeroTouchClass ByName(string name = "Hello Dynamo")
+        {
+            return new ZeroTouchClass();
+        }
+
+        public void Dispose()
+        {
+            // Cleanup here.
+        }
+    }
+}
