c6624088850be30ab25507047be2bf9b0b20c5f2
Matt Jezyk <matt.jezyk@gmail.com>
2014-11-17 22:54:20 -0500

    JoinGeometryUtils.GetJoinedElements

---
+++ src/Libraries/Revit/RevitNodes/Elements/InternalUtilities/JoinGeometryUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.Creation;
+
+using RevitServices.Persistence;
+
+using Document = Autodesk.Revit.DB.Document;
+
+namespace Revit.Elements.InternalUtilities
+{
+    public static class JoinGeometryUtils
+    {
+        public static IList<Element> GetJoinedElements(Document document, Element element)
+        {
+
+            var matches = Autodesk.Revit.DB.JoinGeometryUtils.GetJoinedElements(document, element.InternalElement);
+
+            var instances = matches
+               .Select(x => ElementSelector.ByElementId(x.IntegerValue)).ToList();
+            return instances;
+        }
+    }
+}
